95d333620fd398b7a5a88eab5d94da10c1488fe3
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-24 09:54:41 -0500

    App Set Up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+  
+      <div>
+      <h1>Star Wars API</h1>
+      </div>
+    
+  )
+}
+
+export default App
